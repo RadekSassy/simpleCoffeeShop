8279d1e1a3b378a78fe1aed2ff1500655e03b1b8
RadekSassy <radek@gradepoint.cz>
2024-11-29 07:14:12 +0100

    fix responsivity and fix favicon

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 https://www.youtube.com/watch?v=egQkSOFZNbQ&list=PLnxWKIFbseVAv4xP0mBRycCgwCkm82z16&index=44
 https://github.com/iAmYaxYa/tutorials
 fix responsive design - changed all to be responsive w/o hamburger navbar
+fix favicon
--- scripts/ham.js
+++ scripts/ham.js
@@ -1,9 +1,9 @@
 // ham.js - ovládání hamburger menu
 
-const hamburger = document.querySelector('.hamburger');
-const menuHam = document.querySelector('.menu-ham');
+const hamburger = document.querySelector(".hamburger");
+const menuHam = document.querySelector(".menu");
 
-hamburger.addEventListener('click', () => {
-  menuHam.classList.toggle('active'); // vyjíždění hamburger menu
-  hamburger.classList.toggle('active'); // přepínání třídy pro hamburger tlačítko
+hamburger.addEventListener("click", () => {
+  menuHam.classList.toggle("active"); // vyjíždění hamburger menu
+  hamburger.classList.toggle("active"); // přepínání třídy pro hamburger tlačítko
 });
